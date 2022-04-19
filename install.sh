87dbc7a20ac0a223c87d3c645aa77c621f6f4a4c
Michael John <michael.john@gmx.at>
2022-04-20 00:20:46 +0200

    translations

--- install.sh
+++ install.sh
@@ -6,8 +6,9 @@ sudo cp -v guinotify.png /usr/share/pixmaps/
 sudo cp -v guinotify.desktop /usr/share/applications/
 cp -v guinotify.desktop ~/Desktop/guinotify.desktop
 
-xgettext -d guinotify -o locales/guinotify.pot *.py
+xgettext -d guinotify -o guinotify/locales/guinotify.pot guinotify/*.py
 #poedit 2>/dev/null
+sudo cp guinotify/locales/de/LC_MESSAGES/guinotify.mo /usr/share/locale/de/LC_MESSAGES/
 
 #python setup.py sdist
 #python -m build --wheel --no-isolation
